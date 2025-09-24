cbcb87bbcbecc6daf2b48ed52a44969b083212d9
cnnamon <146155598+cnnacat@users.noreply.github.com>
2025-09-23 22:11:05 -0700

    modifying a apple/linux conditional macro so the lsp works for development when on a windows machine

--- src/backend/server.c
+++ src/backend/server.c
@@ -10,7 +10,8 @@ int main(void)
 }
 
 
-#elif defined(__linux__) || defined(__APPLE__)
+#elif defined(__linux__) || defined(__APPLE__) || defined(_WIN32) 
+// remove defined(_WIN32) before prod, but for now keep it so the lsp works on windows.
 // todo: posix implementation
 
 
