2f5fb803c67a4bd4429cd69e38fae122debc11bd
felixzhengg <212263532+felixzhengg@users.noreply.github.com>
2025-10-02 00:53:33 -0700

    additions to windows task 1 & 2

---
+++ ctrlc_callback.h
@@ -0,0 +1,8 @@
+#ifndef CTRLC_CALLBACK_H
+#define CTRLC_CALLBACK_H
+
+#include <windows.h> 
+
+BOOL WINAPI CtrlCHandler(DWORD ctrlType);
+
+#endif
---
+++ pause.h
@@ -0,0 +1,6 @@
+#ifndef PAUSE_H
+#define PAUSE_H
+
+void pause_program(void);
+
+#endif
