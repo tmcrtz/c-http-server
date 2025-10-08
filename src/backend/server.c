88d346f2caa718e640c3ad7d18cd91c09b29d3eb
felixzhengg <212263532+felixzhengg@users.noreply.github.com>
2025-10-08 00:09:37 -0700

    fixed abstraction issue (hopefully)

--- src/backend/server.c
+++ src/backend/server.c
@@ -4,41 +4,35 @@
 #define IP_ADDRESS "127.0.0.1"
 
 #if _WIN32 
-WSADATA wsaData; 
-
-// function to initialize winsock
-int init_winsock() 
-	{
-    	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    	if (result != 0) 
-			{
-			printf("WSAStartup failed: %d\n", result);
-			return 1;
-			}
-		return 0;
-	}
 
 int main(void)
 {
+	WSADATA wsaData;
+
+	// initialize winsock
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) 
+		{
+		printf("WSAStartup failed: %d\n", result);
+		return 1;
+		}
+
 	// install CTRL-C handler
     if (!SetConsoleCtrlHandler(CtrlHandler, TRUE))
     {
         printf("Error: Could not set control handler.\n");
         return 1;
     }
-	// initialize Winsock
-	if (init_winsock() != 0)
-	{
-		printf("Failed to initialize Winsock.\n");
-		return 1;
-	}
-    printf("Server running...\n");
+
 	printf("Winsock intialized successfully.\n");
+    printf("Server running...\n");
 
     // wait for user input before exiting
     pause_program();
+
 	// clean up Winsock resources
 	WSACleanup();
+	
     // second pause after program exits so that the user can see the exit message before the console window closes
     printf("Exiting program.\n");
     pause_program();
