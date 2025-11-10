6d132322c92ecb71a48037d4dea24d3594f4c977
tmcrtz <126530021+tmcrtz@users.noreply.github.com>
2025-11-10 00:07:08 -0800

    send files (website) to client

--- src/backend/return_file.c
+++ src/backend/return_file.c
@@ -4,28 +4,27 @@ void return_file(char* file_name, int sock_accept)
 {
     char path[512];
 
-    // Serve the main page when requesting "/"
     if (strcmp(file_name, "/") == 0) {
         strcpy(path, "../src/frontend/front_page.html");
     }
+
     else {
-        // Make sure the path starts with '/'
+        // add "/" to end of directory if missing
         char fixed_name[256];
         if (file_name[0] == '/')
             strcpy(fixed_name, file_name);
+
         else {
             fixed_name[0] = '/';
             strcpy(fixed_name + 1, file_name);
         }
-
-        // Build the correct relative path
         snprintf(path, sizeof(path), "../src/frontend%s", fixed_name);
     }
 
     char *file_content = open_file(path);
     if (!file_content) {
         printf("open_file failed for %s\n", path);
-        char *not_found = return_code(404, path, "<h1>404 Not Found</h1>");
+        char *not_found = return_code(404, path, ""); //404 already handled so no need to pass any content
         send(sock_accept, not_found, strlen(not_found), 0);
         free(not_found);
         return;
@@ -36,29 +35,6 @@ void return_file(char* file_name, int sock_accept)
 
     free(file_content);
     free(response);
-
-
-
-
-    // if (!strcmp(file_name, "/")) 
-    // {
-    //     char* file_content = open_file("../src/frontend/front_page.html");
-    //     if (!file_content) {
-    //     printf("open_file failed\n");
-    //     return_code(404, "front_page.html", file_content);
-    //     return;
-    //     }
-        
-    //     char* response = return_code(200, "main_page.html", file_content);
-    //     if (response) 
-    //     {
-    //         send(sock_accept, response, strlen(response), 0);
-    //     }
-
-    //     free(file_content);
-    //     free(response);
-    // }
-    // //add directory navigation
 }
 
 char* open_file(char* file_dir) 
